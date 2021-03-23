3dea7bce53a208cd41b0802a14f78a98bf181b21
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-22 21:40:52 -0300

    Estilização com SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-export default function Home() {
+import styles from '../styles/home.module.scss';
+ export default function Home() {
   return (
-    <h1>Hello World!!</h1>
+    <h1 className={styles.title}>Hello World!!</h1>
   )
 }
