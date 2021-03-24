78f4394369f7151974e14674dab341d6e12dc7f4
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-23 21:06:41 -0300

    Componente: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -14,7 +15,10 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access ti all the publications <br />
             <span>for $9.99 month</span>
+
           </p>
+          <SubscribeButton />
+
         </section>
         <img src='/images/avatar.svg' alt='Girl Coding' />
       </main>
