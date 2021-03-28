674e94886c2a514bda160003ffefe7580703729f
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-27 23:38:09 -0300

    Evitando duplicação no Stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user  // console.log(user)
-      
+
       try {
         await fauna.query(
           q.If(
