a101e83753d5c28f915dd80bdba30089ce69daae
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-29 20:53:42 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,8 +5,20 @@ import styles from './styles.module.scss'
 import { GetStaticProps } from 'next'
 import { getPrismicClient } from '../../services/prismic'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updateAt: string;
+
+}
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -14,29 +26,16 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a>
-                        <time>29 de marco 2020</time>
-                        <strong>Titulo do post</strong>
-                        <p>lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet</p>
-                    </a>
+                    {posts.map(post => (
+                        <a key={post.slug}>
+                            <time>{post.updateAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
 
-                    <a>
-                        <time>29 de marco 2020</time>
-                        <strong>Titulo do post</strong>
-                        <p>lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet</p>
-                    </a>
 
-                    <a>
-                        <time>29 de marco 2020</time>
-                        <strong>Titulo do post</strong>
-                        <p>lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet</p>
-                    </a>
 
-                    <a>
-                        <time>29 de marco 2020</time>
-                        <strong>Titulo do post</strong>
-                        <p>lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet</p>
-                    </a>
                 </div>
             </main>
         </>
@@ -44,19 +43,32 @@ export default function Posts() {
 }
 
 
-export const getStaticProps: GetStaticProps = async() =>{
+export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
     const response = await prismic.query([
         Prismic.predicates.at('document.type', 'post')
-    ],{
+    ], {
         fetch: ['post.title', 'post.content'],
-        pageSize:3,
+        pageSize: 3,
     })
 
     // console.log(response)
-    console.log(JSON.stringify(response, null, 2))
-    return{
-        props:{}
+    // console.log(JSON.stringify(response, null, 2))
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',
+            updateAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        }
+    })
+    return {
+        props: { posts }
     }
 }
