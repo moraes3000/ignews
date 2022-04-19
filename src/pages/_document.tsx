fdc7efa9c236cf9bba55621047a0d09d935c3827
Bruno B. Moraes <bruno_bmoraes@hotmail.com>
2022-04-19 15:08:17 -0300

    correção deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/Document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="pt-BR">
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
-                    <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
                 <body>
                     <Main />
@@ -16,4 +17,4 @@ export default class MyDocument extends Document {
             </Html>
         )
     }
-}
+} 
